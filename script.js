c746c1c07ee1eb5bf672d17837b3cbebc2c05a55
Lorenzo Candiago <lorenzocandiago@hotmail.com>
2021-11-24 02:01:37 -0300

    Reveal friend feature

--- script.js
+++ script.js
@@ -1,32 +1,60 @@
-let friends = []
-let possibleFriends = []
+const formSectionElement = document.getElementById("form-section")
 
-const getRandomFriendAndRemove = (exceptFriend) => {
-  const randomNumber = Math.floor(Math.random() * friends.length)
-  const randomFriend = friends[randomNumber]
-  if (randomFriend === exceptFriend) {
-    return getRandomFriendAndRemove(exceptFriend)
+const urlParams = new URLSearchParams(window.location.search);
+const friendParam = urlParams.get('f')
+
+if (friendParam) {
+  const secretSectionElement = document.getElementById("secret-section")
+  const currentPersonElement = document.getElementById("current-person")
+  const revealButtonElement = document.getElementById("reveal-button")
+  const secretFriendElement = document.getElementById("secret-friend")
+
+  formSectionElement.classList.add('d-none')
+  secretSectionElement.classList.remove('d-none')
+
+  const decodedFriendParam = window.atob(friendParam)
+  const [current, friend] = decodedFriendParam.split(':')
+
+  currentPersonElement.innerText = current
+  revealButtonElement.addEventListener('click', () => {
+    secretFriendElement.innerText = friend
+  })
+} else {
+  let friends = []
+  let possibleFriends = []
+
+  const getRandomFriendAndRemove = (exceptFriend) => {
+    const randomNumber = Math.floor(Math.random() * friends.length)
+    const randomFriend = friends[randomNumber]
+    if (randomFriend === exceptFriend) {
+      return getRandomFriendAndRemove(exceptFriend)
+    }
+    friends.splice(randomNumber, 1)
+    return randomFriend
   }
-  friends.splice(randomNumber, 1)
-  return randomFriend
-}
 
-const friendsElement = document.getElementById("participantes")
-const resultElement = document.getElementById("resultado")
-const sortFormElement = document.getElementById("sort-form")
+  const friendsElement = document.getElementById("participantes")
+  const resultElement = document.getElementById("resultado")
+  const sortFormElement = document.getElementById("sort-form")
+  const resultSectionElement = document.getElementById("result-section")
 
-const sortFriends = () => {
-  friends = friendsElement.value.split(',')
-  possibleFriends = [...friends]
+  const sortFriends = () => {
+    friends = friendsElement.value.split(',')
+    possibleFriends = [...friends]
 
-  possibleFriends.forEach(friend => {
-    const randomFriend = getRandomFriendAndRemove(friend)
+    possibleFriends.forEach(friend => {
+      const randomFriend = getRandomFriendAndRemove(friend)
 
-    resultElement.innerHTML += `<tr><td>${friend}</td><td>https://secretoamigo.com.br?f=${window.btoa(friend + '.' + randomFriend)}</td></tr>`
+      resultElement.innerHTML += `<tr><td>${friend}</td><td>https://secretoamigo.com.br?f=${window.btoa(friend + ':' + randomFriend)}</td></tr>`
+    })
+  }
+
+  sortFormElement.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    formSectionElement.classList.add('d-none')
+    resultSectionElement.classList.remove('d-none')
+
+    sortFriends()
   })
 }
-
-sortFormElement.addEventListener('submit', (event) => {
-  event.preventDefault()
-  sortFriends()
-})
